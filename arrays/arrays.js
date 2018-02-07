51c21f5acffa4c3f49a608f0db00e0113f0a82af
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-07 02:57:27 -0600

    Fix arrays.html if statement to capture all posible errors.

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,11 +1,13 @@
+var numbersArray = ['1-Chris','2-Sergey','3-Donald','4-Tish','5-Ian','6-Cam','7-Mariam','8-David','9-Thais'];
 
-var numbersArray = ['1','2','3','4','5','6','7','8','9'];
-document.write("<br>")
 
-var desiredItem = prompt("Given the array containing ['1','2','3','4','5','6','7','8','9'] \nEnter the array's desired item (NOT INDEX) to access: ");
+var desiredItem = prompt("Given the array containing\n'1-Chris'\n'2-Sergey'\n'3-Donald'\n'4-Tish'\n'5-Ian'\n'6-Cam'\n'7-Mariam'\n'8-David'\n'9-Thais']\nEnter the array's desired item (1-9) to access:\n(NOT INDEX)");
+
 desiredItem = parseInt(desiredItem);
-if(desiredItem > numbersArray.length || desiredItem === 0) {
-    document.write("You selection was invalid");
+if(desiredItem > numbersArray.length || desiredItem === 0 || isNaN(desiredItem) || desiredItem < 0) { //evaluate user entry
+    document.write("<br>") //Print a new line
+    document.write("You selection was invalid, it should have been a number between 1 and 9.");
 } else {
-    document.write("You selected item: " + numbersArray[desiredItem-1]);
+    document.write("<br>") //Print a new line
+    document.write("You selected item was: " + numbersArray[desiredItem-1]);
 }
