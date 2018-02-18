3a559346e6027f3706fb709b0fc602d981785253
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-18 00:49:22 -0600

    Add win/loss functionality

--- crystalgame/main.js
+++ crystalgame/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var sum=0;
   var hiddenValue;
   var userClickedVal;
+  var wins=0;
+  var losses = 0;
   //Generate a random number for the pc
   function randomMatch() {
     randomMatch = Math.floor(Math.random() * 80) + 10;
@@ -27,7 +29,15 @@ $(document).ready(function() {
         userClickedVal = parseInt($(this).attr("hiddenValue"));
         console.log(userClickedVal);
         sum += userClickedVal;
-
+        if (sum > randomMatch) {
+            losses++;
+            $(".items").addClass("active");
+            $("#lossesholder").text(losses);
+        } else if ( sum === randomMatch) {
+            wins++;
+            $(".items").addClass("active");
+            $("#winsholder").text(wins);
+        }
         $("#total").text(sum);
   });
 
