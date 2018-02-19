6b2f7362b89433e8d0052f3afafca5ac7a35099b
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-18 22:06:58 -0600

    Everything working but labels and UI optimization

--- crystalgame/main.js
+++ crystalgame/main.js
@@ -1,47 +1,68 @@
 $(document).ready(function() {
-  var randomMatch;
-  var sum=0;
-  var hiddenValue;
-  var userClickedVal;
-  var wins=0;
+  var wins =0
   var losses = 0;
-  //Generate a random number for the pc
-  function randomMatch() {
-    randomMatch = Math.floor(Math.random() * 80) + 10;
-    return randomMatch;
+  var randomMatch=0;
+  var hiddenValue=0;
+  var sum = 0;
+  var userClickedVal=0;
+
+  function play() {
+    //Generate a random numberfunction randomMatch() {
+    randomMatch = Math.floor(Math.random() * 102) + 19;
+    $("#matchme").text(randomMatch);
+    console.log('random match PC ' + randomMatch);
+    //Generate the hidden values for the crystals
+    function addHiddenValues() {
+      for (i = 1; i < 5; i++) {
+        hiddenValue = Math.floor(Math.random() * 12) + 1; //Crystal Random #
+        $("#item" + i).attr("hiddenValue", hiddenValue)[0];
+      }
+    }
+    addHiddenValues();
   }
-  //
-  randomMatch();
-  $("#matchme").text(randomMatch);
 
-  function randomImgVals () {
-    hiddenValue = Math.floor(Math.random() * 15 ) + 1;
-    return hiddenValue;
-  }
+  play();
 
-  for(i=1; i<5; i++) {
-      randomImgVals();
-      $("#item"+i).attr("hiddenValue",hiddenValue)[0];
-  }
+  // //Evaluate each click from the user and add it to a sum
+  $(".items").on("click", function() {
+    userClickedVal = parseInt($(this).attr("hiddenValue"));
+    console.log('the crystal value was ' + userClickedVal);
+    sum += userClickedVal;
+    $("#total").text(sum);
+    console.log("sum INSIDE :" + sum);
+    if (sum > randomMatch) {
+      losses++;
+      $("#lossesholder").text(losses);
+      console.log("YOU LOSS !!!!!! :" + sum);
+      deactivateCrystals();
+    } else if (sum === randomMatch) {
+      wins++;
+      $("#winsholder").text(wins);
+      console.log("YOU WON !!!!!! :" + sum);
+      deactivateCrystals();
+    }
 
+    function deactivateCrystals() {
+      $(".items").addClass("active");
+      setTimeout(playAgain, 5000);
+    }
 
-  $(".items").on("click", function () {
-        userClickedVal = parseInt($(this).attr("hiddenValue"));
-        console.log(userClickedVal);
-        sum += userClickedVal;
-        if (sum > randomMatch) {
-            losses++;
-            $(".items").addClass("active");
-            $("#lossesholder").text(losses);
-        } else if ( sum === randomMatch) {
-            wins++;
-            $(".items").addClass("active");
-            $("#winsholder").text(wins);
-        }
-        $("#total").text(sum);
   });
 
-
+  function playAgain() {
+    //console.log("sum after win/loss: " + sum);
+    $(".items").removeClass("active");
+    $("#total").empty();
+    $("#matchme").empty();
+    randomMatch =0;
+    hiddenValue=0;
+    sum=0;
+    userClickedVal=0;
+    play();
+    console.log("-----------------------------");
+    console.log("---A NEW GAME JUST STARTED----");
+    console.log("-----------------------------");
+    console.log("match AFTER WIN/LOSS : " + randomMatch);
+  }
 
 });
-
